37a6ef55a8ef40ffb59494cad1c9619c7a4c688b
xJuhix <49311688+xJuhix@users.noreply.github.com>
2021-02-16 23:24:58 +0100

    Fixing index page
    Trying to add background image, fixing CORS/CORB error and moving blockquote to the center

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -5,12 +5,14 @@ import { Link } from 'react-router-dom';
 import Heading from '../layout/Heading';
 import Row  from 'react-bootstrap/Row';
 import Col  from 'react-bootstrap/Col';
+import APOD from '../nasa/APOD';
+import moonImg from '../../images/moonimg.png';
 
 
 export function Home() {
   return (
 		<>
-    <div className="background-image" styles={{ backgroundImage:`url(space.png)` }}>
+    <div className="background-image" style={{ backgroundImage: `src(${moonImg})` }}>>
       <Helmet>
         <title>Home</title>
         <meta
@@ -18,6 +20,29 @@ export function Home() {
           content="Gravity0 is a microsite for NASA"
         />
       </Helmet>
+
+			<Row>
+				<Col>
+				<div className="page-intro">
+					<Heading title="Gravity0" />
+						<h2>Microsite for NASA</h2>
+							<p>
+								Here you will find NASA's active and upcoming missions and programs, the technology that is being devloped for the mission and programs is revolutionary.
+								NASA has over the years devloped partnerships with other space agencies both private and government owned. So we as a world have come together to explore the endless unknown.
+								The purpose of GravityZero is to infrom the public about NASA's currently active space programs and missions as well as upcoming ones. We would like to share our knowledge about NASA's international collaborations.
+							</p>
+        
+							<blockquote className="page__blockquote">
+              <q>
+								Curiosity is the essence of our excistence
+              </q>
+              <footer className="blockquote-footer">
+              	Gene Cernan
+              </footer>
+            </blockquote>
+        </div>
+				</Col>
+			</Row>
 
       <div className="explore__buttons">
 
@@ -56,35 +81,7 @@ export function Home() {
         </Link>
 
       </div>
-
-			<Row>
-				<Col>
-				<div className="page-intro">
-					<Heading title="Gravity0" />
-						<h2>Microsite for NASA</h2>
-							<p>
-								Here you will find NASA's active and upcoming missions and programs, the technology that is being devloped for the mission and programs is revolutionary.
-								NASA has over the years devloped partnerships with other space agencies both private and government owned. So we as a world have come together to explore the endless unknown.
-								The purpose of GravityZero is to infrom the public about NASA's currently active space programs and missions as well as upcoming ones. We would like to share our knowledge about NASA's international collaborations.
-							</p>
-        
-							<blockquote className="page__blockquote">
-              <q>
-								Curiosity is the essence of our excistence
-              </q>
-              <footer className="blockquote-footer">
-              	Gene Cernan
-              </footer>
-            </blockquote>
-        </div>
-				</Col>
-				<Col>
-
-					<p>Add img of the day + description</p>
-
-				</Col>
-			</Row>
-
+				<APOD />
     </div>
 		</>
   );
--- src/constants/api.js
+++ src/constants/api.js
@@ -1,3 +1,3 @@
 export const BASE_URL = 'https://api.nasa.gov/planetary/apod?api_key=VusA3SIQIB7epib5IM31ZlGQErT1UgVdF7qUuL06';
-
+export const IMG_Today = `https://api.nasa.gov/planetary/apod`;
 
