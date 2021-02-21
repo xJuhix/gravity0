7a50a43f81f9b4cdded51f90491f2b7c94b71a76
xJuhix <49311688+xJuhix@users.noreply.github.com>
2021-02-21 12:55:58 +0100

    Updating Styling

--- src/components/upcomingevents/Event.js
+++ src/components/upcomingevents/Event.js
@@ -2,6 +2,8 @@
 /* eslint-disable jsx-a11y/alt-text */
 /* eslint-disable react/jsx-no-target-blank */
 import React, { useState, useEffect } from "react";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 import { EVENTS } from "../../constants/api";
 
 function Events() {
@@ -25,27 +27,33 @@ function Events() {
         {events && (
           <>
             {events.map((event) => (
-              <div className="event" key={event.name}>
-                <img src={`${event.feature_image}`} />
-                <div className="event-info">
-                  <div className="event-minor">
-                    <span>{event.type.name}</span>
-                    {`${new Date(event.date).toLocaleDateString("en-gb", {
-                      year: "numeric",
-                      month: "long",
-                      day: "numeric",
-                      timeZone: "utc",
-                    })},
-									   ${Intl.DateTimeFormat("en", { hour: "numeric" })
-                       .format(new Date(event.date))
-                       .toString()}`}
+              <Col>
+                <div className="event" key={event.name}>
+                  <img src={`${event.feature_image}`} />
+                  <div className="event-info">
+                    <Row className="event-minor">
+                      <Col>
+                        <p className="event-type">{event.type.name}</p>
+                      </Col>
+                      <Col>
+                        <p>
+                          {`${new Date(event.date).toLocaleDateString("en-gb", {
+                            year: "numeric",
+                            month: "long",
+                            day: "numeric",
+                            timeZone: "utc",
+                          })}
+									  			`}
+                        </p>
+                      </Col>
+                    </Row>
+                    <a className="event-title" href={`${event.news_url}`}>
+                      <h3>{event.name}</h3>
+                    </a>
+                    <p>{event.description}</p>
                   </div>
-                  <a className="event-title" href={`${event.news_url}`}>
-                    {event.name}
-                  </a>
-                  <p>{event.description}</p>
                 </div>
-              </div>
+              </Col>
             ))}
           </>
         )}
